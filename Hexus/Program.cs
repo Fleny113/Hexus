d01443d3c2d7329e9dcfca374d37d1912e44cca8
Fleny <fleny113@outlook.com>
2023-11-13 18:49:15 +0100

    Add Hexus.csproj project

---
+++ Hexus/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.CommandLine;
+
+var nameArgument = new Argument<string>("name", "The name of the application to manage");
+var forceOption = new Option<bool>("--force", "Force the stop of the application");
+forceOption.AddAlias("-f");
+
+var startCommand = new Command("stop") { nameArgument, forceOption };
+
+startCommand.SetHandler((name, force) =>
+{
+    var toForceStop = force ? "force" : "";
+    
+    Console.WriteLine($"Requested to {toForceStop}stop an application named: \"{name}\"");
+}, nameArgument, forceOption);
+
+var rootCommand = new RootCommand();
+rootCommand.AddCommand(startCommand);
+
+return await rootCommand.InvokeAsync(args);
