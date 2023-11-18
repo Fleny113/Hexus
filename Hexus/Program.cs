240961c5aeed1af8a7a637b28da2b7a6c8ee20d5
Fleny <fleny113@outlook.com>
2023-11-18 21:17:30 +0100

    Convert stop test command to class

--- Hexus/Program.cs
+++ Hexus/Program.cs
@@ -1,19 +1,8 @@
-﻿using System.CommandLine;
-
-var nameArgument = new Argument<string>("name", "The name of the application to manage");
-var forceOption = new Option<bool>("--force", "Force the stop of the application");
-forceOption.AddAlias("-f");
-
-var startCommand = new Command("stop") { nameArgument, forceOption };
-
-startCommand.SetHandler((name, force) =>
-{
-    var toForceStop = force ? "force" : "";
-    
-    Console.WriteLine($"Requested to {toForceStop}stop an application named: \"{name}\"");
-}, nameArgument, forceOption);
+﻿using Hexus;
+using System.CommandLine;
 
 var rootCommand = new RootCommand();
-rootCommand.AddCommand(startCommand);
+
+rootCommand.AddCommand(StopCommand.Command);
 
 return await rootCommand.InvokeAsync(args);
---
+++ Hexus/StopCommand.cs
@@ -0,0 +1,35 @@
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+namespace Hexus;
+
+internal static class StopCommand
+{
+    private static readonly Argument<string> NameArgument = new("name", "The name of the application to stop");
+    private static readonly Option<bool> ForceOption = new("--force", "Force the stop of the application");
+    
+    public static readonly Command Command = new("stop", "Stop an application")
+    {
+        NameArgument,
+        ForceOption,
+    };
+
+    static StopCommand()
+    {
+        ForceOption.AddAlias("-f");
+        
+        Command.SetHandler(HandleAsync);
+    }
+    
+    private static Task HandleAsync(InvocationContext context)
+    {
+        var name = context.ParseResult.GetValueForArgument(NameArgument);
+        var force = context.ParseResult.GetValueForOption(ForceOption);
+        
+        var toForceStop = force ? "force" : "";
+    
+        Console.WriteLine($"Requested to {toForceStop}stop an application named: \"{name}\"");
+
+        return Task.CompletedTask;
+    }
+}
