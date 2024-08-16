e037726bce7aed52890d62bca674ae3b594e8283
Fleny <fleny113@outlook.com>
2024-08-16 21:51:16 +0200

    Add daemon status command

--- Hexus/Commands/DaemonCommand.cs
+++ Hexus/Commands/DaemonCommand.cs
@@ -12,19 +12,24 @@ internal static class DaemonCommand
         Arity = ArgumentArity.ZeroOrMore,
     };
 
+    private static readonly Option<bool> ShowSocketPath = new("--socket", "Show the socket file being used. Useful for debugging");
+
     private static readonly Command StartSubCommand = new("start", "Start the Hexus daemon") { DaemonOptions };
     private static readonly Command StopSubCommand = new("stop", "Stop the currently running Hexus daemon");
+    private static readonly Command StatusSubCommand = new("status", "Gets the current status of the Hexus daemon") { ShowSocketPath };
 
     public static readonly Command Command = new("daemon", "Manage the Hexus daemon")
     {
         StartSubCommand,
         StopSubCommand,
+        StatusSubCommand,
     };
 
     static DaemonCommand()
     {
         StartSubCommand.SetHandler(StartSubCommandHandler);
         StopSubCommand.SetHandler(StopSubCommandHandler);
+        StatusSubCommand.SetHandler(StatusSubCommandHandler);
     }
 
     private static async Task StartSubCommandHandler(InvocationContext context)
@@ -60,5 +65,20 @@ internal static class DaemonCommand
         }
 
         PrettyConsole.Out.MarkupLine("[indianred1]Daemon[/] stopped.");
+    }
+
+    private static async Task StatusSubCommandHandler(InvocationContext context)
+    {
+        var showSocket = context.ParseResult.GetValueForOption(ShowSocketPath);
+        var ct = context.GetCancellationToken();
+
+        var isRunning = await HttpInvocation.CheckForRunningDaemon(ct);
+        
+        if (showSocket)
+        {
+            PrettyConsole.Out.MarkupLineInterpolated($"[gray]Socket being used: [link]{Configuration.HexusConfiguration.UnixSocket}[/][/]");
+        }
+
+        PrettyConsole.Out.MarkupLine($"The daemon is {(isRunning ? "[springgreen1]running[/]" : "[indianred1]not running[/]")}.");
     }
 }
