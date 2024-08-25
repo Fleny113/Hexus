c31d9221809135a3215fabfa412ff93769460f25
Fleny <fleny113@outlook.com>
2024-08-25 09:42:53 +0200

    Check for .bat and .cmd files on Windows
    .bat and .cmd can be started directly without a cmd to support them. NPM is an example of program that you can now start with this

--- Hexus/PathHelper.cs
+++ Hexus/PathHelper.cs
@@ -5,28 +5,28 @@ internal static class PathHelper
 {
     public static string ResolveExecutable(string executable)
     {
-        // relative folders resolver (./.../exe)
+        // relative folders resolver (./../exe)
         var absolutePath = EnvironmentHelper.NormalizePath(executable);
 
         if (File.Exists(absolutePath))
             return absolutePath;
 
         // PATH env resolver
-        if (executable.Contains('/') || executable.Contains('\\'))
+        if (executable.Contains(Path.DirectorySeparatorChar))
             throw new Exception("Executable cannot have slashes");
 
         var pathEnv = Environment.GetEnvironmentVariable("PATH") ?? throw new Exception("Cannot get PATH environment variable");
 
         // Linux and Windows use different split char for the path
-        var paths = pathEnv.Split(OperatingSystem.IsWindows() ? ';' : ':');
+        var paths = pathEnv.Split(Path.PathSeparator);
 
         var resolvedExecutable = paths
             .SelectMany<string, string>(path =>
             {
                 var file = Path.Combine(path, executable);
 
-                // On windows we want to check for either .exe or .com files
-                if (OperatingSystem.IsWindows()) return [$"{file}.exe", $"{file}.com"];
+                // On windows we want to check for either .exe, .com, .bat or .cmd files
+                if (OperatingSystem.IsWindows()) return [$"{file}.exe", $"{file}.com", $"{file}.bat", $"{file}.cmd"];
 
                 return [file];
             })
